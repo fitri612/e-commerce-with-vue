36ad044896e4776310cd3c8811f563bbf5e385bf
Hay I'am Inces <74086121+fitri612@users.noreply.github.com>
2022-04-23 13:44:32 +0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <HeaderShop/>
+    <HeroShop/>
+    <WomenShop/>
+    <InstaShop/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShop from '@/components/HeaderShop.vue'
+import HeroShop from '@/components/HeroShop.vue'
+import WomenShop from '@/components/WomenShop.vue'
+import InstaShop from '@/components/InstaShop.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShop,
+    HeroShop,
+    WomenShop,
+    InstaShop
+  }
+};
+</script>
+
+
