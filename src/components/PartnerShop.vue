d06b62b899f09969fa741351f6aa23acb1c9f37e
Hay I'am Inces <74086121+fitri612@users.noreply.github.com>
2022-04-23 14:14:06 +0700

    Update PartnerShop.vue

--- src/components/PartnerShop.vue
+++ src/components/PartnerShop.vue
@@ -2,7 +2,7 @@
         <!-- Partner Logo Section Begin -->
     <div class="partner-logo">
         <div class="container">
-            <carousel class="logo-carousel">
+            <carousel class="logo-carousel" :loop="true" :autoplay="true" :items="5" :dots="false" :nav="false">
                 <div class="logo-item">
                     <div class="tablecell-inner">
                         <img src="img/logo-carousel/logo-1.png" alt="" />
